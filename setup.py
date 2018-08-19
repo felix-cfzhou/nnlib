2c4f6d223a38c44ebceda7c46819342a86b26a90
fzhou <felix990302@yahoo.ca>
2018-08-18 23:26:04 -0400

    implemented setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(name='nnlib',
+      version='0.1',
+      description='numpy only neural network library',
+      author='Felix Zhou',
+      author_email='felix990302@yahoo.ca',
+      packages=['nnlib'],
+     )
