e7a38d71276d15567a15bb7f6d63a2e201fcccbb
fzhou <felix990302@yahoo.ca>
2018-08-18 23:11:05 -0400

    added test for sigmoid

---
+++ tests/utils/activation.py
@@ -0,0 +1,13 @@
+from numpy.testing import assert_allclose
+from numpy import array
+
+from utils.activation import sigmoid, relu
+
+
+def test_sigmoid():
+    arr = sigmoid(array([[1, 2]]))
+
+    assert_allclose(
+            arr,
+            array([[0.73105858, 0.88079708]])
+            )
