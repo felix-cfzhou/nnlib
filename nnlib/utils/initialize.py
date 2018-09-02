c5330e8cb90b32f1835b563f4cf393f7cc78d785
fzhou <felix990302@yahoo.ca>
2018-09-02 14:59:35 -0400

    added initialization func defaulting to He initialization

--- nnlib/utils/initialize.py
+++ nnlib/utils/initialize.py
@@ -1,7 +1,19 @@
 import numpy as np
 
 
-def initialize_parameters(layers_dims):
+def he_initialization(layers_dims, layer):
+    """
+    Arguments:
+    layer_dims -- python list containing tuples of dimensions of each layer in the network
+    layer -- layer number of current layer
+
+    Returns:
+    constant -- numerical constant used to scale the initialized weights
+    """
+    return np.sqrt(2 / layers_dims[layer-1])
+
+
+def initialize_parameters(layers_dims, initialization_func=he_initialization):
     """
     Arguments:
     layer_dims -- python list containing tuples of dimensions of each layer in the network
@@ -14,7 +26,7 @@ def initialize_parameters(layers_dims):
     L = len(layers_dims)
 
     for l in range(1, L):
-        parameters["W"][l] = np.random.randn(layers_dims[l], layers_dims[l-1])*0.01
+        parameters["W"][l] = np.random.randn(layers_dims[l], layers_dims[l-1])*initialization_func(layers_dims, l)
         parameters["b"][l] = np.zeros((layers_dims[l], 1))
 
     return parameters
